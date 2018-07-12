31d10cb1ae2636266ebfd1e9935c1285df32861b
Radivarig <reslav.hollos@gmail.com>
2018-07-12 02:02:16 +0200

    Add define, it, expect example

---
+++ src/example.js
@@ -0,0 +1,4 @@
+module.exports = {
+  identity: (x) => x,
+  abs: (x) => x < 0 ? -x: x,
+}
---
+++ src/example.spec.js
@@ -0,0 +1,17 @@
+const example = require('./example.js')
+
+define ('example', () => {
+    define ('identity', () => {
+    it ('should return the same value', () => {
+      const val = 5
+      expect (example.identity (val)).toBe (val)
+    })
+    define ('abs', () => {
+      it ('should return absolute of a value', () => {
+        const val = 5
+        expect (example.abs (val)).toBe (val, 'should be same')
+        expect (example.abs (-val)).toBe (val, 'should have positive sign')
+      })
+    })
+  })
+})
