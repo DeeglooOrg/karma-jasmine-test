bea430a56375185f995df63e379127fbcff0a156
Radivarig <reslav.hollos@gmail.com>
2018-07-17 17:03:16 +0200

    Fix nesting

--- src/simple.example.spec.js
+++ src/simple.example.spec.js
@@ -6,12 +6,12 @@ describe ('example', () => {
       const anyVal = 5
       expect (example.identity (anyVal)).toBe (anyVal)
     })
-    describe ('abs', () => {
-      it ('should return absolute of a value', () => {
-        const positiveVal = 5
-        expect (example.abs (positiveVal)).toBe (positiveVal, 'should be same')
-        expect (example.abs (-positiveVal)).toBe (positiveVal, 'should have positive sign')
-      })
+  })
+  describe ('abs', () => {
+    it ('should return absolute of a value', () => {
+      const positiveVal = 5
+      expect (example.abs (positiveVal)).toBe (positiveVal, 'should be same')
+      expect (example.abs (-positiveVal)).toBe (positiveVal, 'should have positive sign')
     })
   })
 })
